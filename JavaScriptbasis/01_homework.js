012a9e51ce24c82b0d065fe382c01c70254fbc87
phoenixzk <461028592@qq.com>
2020-08-13 11:22:58 +0800

    JavaScriptbasis

---
+++ JavaScriptbasis/01_homework.js
@@ -0,0 +1,20 @@
+var r=5;
+const PI=3.14;
+//计算周长
+var length=2*PI*r;
+console.log(length);
+//计算30弧度
+console.log(30/360*length);
+console.log(60/360*length);
+console.log(90/360*length);
+
+var price1=12.5;
+var count1=20;
+var price2=30;
+var count2=8.5;
+console.log(price1*count1+price2*count2);
+
+
+
+
+
